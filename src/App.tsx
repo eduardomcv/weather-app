4d6a90c1722e5f8829e1667746e5886a9f970678
Eduardo Vicente <eduardomcv42@gmail.com>
2020-07-18 16:50:50 +0100

    added main content + styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React, { FunctionComponent } from 'react';
 import Header from './components/Header';
+import MainContent from './components/MainContent';
 
 const App: FunctionComponent = () => (
   <>
     <Header />
+    <MainContent />
   </>
 );
 
---
+++ src/components/MainContent/MainContent.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react';
+import './MainContent.css';
+
+const MainContent: FunctionComponent = () => (
+  <main className="main-content">
+    <div />
+  </main>
+);
+
+export default MainContent;
