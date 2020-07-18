f74ee263cfd814a9fc1670b86680adb64234fb6d
Eduardo Vicente <eduardomcv42@gmail.com>
2020-07-18 16:26:41 +0100

    added header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react';
+import './Header.css';
+
+const Header: FunctionComponent = () => (
+  <header className="header">
+    <h1 className="heading">
+      Weather app
+    </h1>
+  </header>
+);
+
+export default Header;
