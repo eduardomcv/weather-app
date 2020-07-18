fb5c8b91bc695c44e1a65098278697eccb4881ac
Eduardo Vicente <eduardomcv42@gmail.com>
2020-07-18 16:31:36 +0100

    fixed import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import Header from './components/Header/Header';
+import Header from './components/Header';
 
 const App: FunctionComponent = () => (
   <>
