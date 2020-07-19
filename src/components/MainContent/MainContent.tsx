8b1ad40af6bf1c371e142b88cc54d7c2f76bc082
Eduardo Vicente <eduardomcv42@gmail.com>
2020-07-19 15:51:59 +0100

    added location type, added temperature state. fixed on change type

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,15 +1,20 @@
 import React, { FunctionComponent, useState } from 'react';
 import LocationSelector from '../LocationSelector';
+import { TemperatureUnit, Location } from '../../sharedTypes';
 import './MainContent.scss';
 
 const MainContent: FunctionComponent = () => {
-  const [location, setLocation] = useState<string>('lisbon');
+  const [location, setLocation] = useState<Location>('lisbon');
+  const [temperatureUnit, setTemperatureUnit] = useState<TemperatureUnit>('celsius');
 
   return (
     <main className="main-content">
       <LocationSelector
-        onChange={(e) => setLocation(e.target.value)}
         value={location}
+        onChange={(event) => {
+          const value = event.target.value as Location;
+          setLocation(value);
+        }}
       />
     </main>
   );
