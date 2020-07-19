3889fbbc74fe591528d44c592ae5920f571e074a
Eduardo Vicente <eduardomcv42@gmail.com>
2020-07-19 08:29:33 +0100

    passes props to selector

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,11 +1,18 @@
-import React, { FunctionComponent } from 'react';
-import './MainContent.scss';
+import React, { FunctionComponent, useState } from 'react';
 import LocationSelector from '../LocationSelector';
+import './MainContent.scss';
 
-const MainContent: FunctionComponent = () => (
-  <main className="main-content">
-    <LocationSelector />
-  </main>
-);
+const MainContent: FunctionComponent = () => {
+  const [location, setLocation] = useState<string>('lisbon');
+
+  return (
+    <main className="main-content">
+      <LocationSelector
+        onChange={(e) => setLocation(e.target.value)}
+        value={location}
+      />
+    </main>
+  );
+};
 
 export default MainContent;
