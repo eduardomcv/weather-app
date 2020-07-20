3eb2ae7d3672702bb50505052943756c6e90b0d0
Eduardo Vicente <eduardomcv42@gmail.com>
2020-07-20 23:12:37 +0100

    renamed config maps

--- src/config.ts
+++ src/config.ts
@@ -4,13 +4,13 @@ export const OPEN_WEATHER_API_KEY = process.env.REACT_APP_API_KEY;
 
 export const OPEN_WEATHER_API_ENDPOINT = 'https://api.openweathermap.org/data/2.5/weather';
 
-export const LOCATION_VALUE_TO_LOCATION_QUERY_PARAM_MAP: Record<Location, string> = {
+export const LOCATION_MAP: Record<Location, string> = {
   lisbon: 'lisbon,pt',
   newyork: 'new+york,us',
   london: 'london,uk',
 };
 
-export const TEMPERATURE_UNIT_VALUE_TO_UNITS_QUERY_PARAM_MAP: Record<TemperatureUnit, string> = {
+export const TEMPERATURE_UNIT_MAP: Record<TemperatureUnit, string> = {
   celsius: 'metric',
   farenheit: 'imperial',
 };
