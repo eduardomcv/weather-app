9b72afd1b6fb62df5d3a5490d16d88dfd47aeb08
Eduardo Vicente <eduardomcv42@gmail.com>
2020-07-21 00:50:54 +0100

    added icons endpoint

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,8 @@ export const OPEN_WEATHER_API_KEY = process.env.REACT_APP_API_KEY;
 
 export const OPEN_WEATHER_API_ENDPOINT = 'https://api.openweathermap.org/data/2.5/weather';
 
+export const OPEN_WEATHER_ICONS_ENDPOINT = 'http://openweathermap.org/img/wn/';
+
 export const LOCATION_MAP: Record<Location, string> = {
   lisbon: 'lisbon,pt',
   newyork: 'new+york,us',
